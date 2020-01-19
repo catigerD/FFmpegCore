247cbf6457a9f1f7af606eed0e556294dc675e8e
dengchong.999 <dengchong.999@bytedance.com>
2020-01-19 22:24:47 +0800

    feat: FFmpegExerciseTest

---
+++ src/ffmpeg_exercise/FFmpegExerciseTset.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dengchong on 2020-01-19.
+//
+
+#include "FFmpegExerciseTset.h"
---
+++ src/ffmpeg_exercise/FFmpegExerciseTset.h
@@ -0,0 +1,14 @@
+//
+// Created by dengchong on 2020-01-19.
+//
+
+#ifndef FFMPEGCORE_FFMPEGEXERCISETSET_H
+#define FFMPEGCORE_FFMPEGEXERCISETSET_H
+
+
+class FFmpegExerciseTset {
+
+};
+
+
+#endif //FFMPEGCORE_FFMPEGEXERCISETSET_H
